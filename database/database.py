0654b1c69df0a2b0112d9376f15e191a65465eb4
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2023-12-10 13:25:23 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -8,7 +8,7 @@ import asyncio
 
 from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, UniqueConstraint, func
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://xajay10997:Xr1p2CNHjIJLrHl8@cluster0.bjsdwy9.mongodb.net/?retryWrites=true&w=majority")
 
 def start() -> scoped_session:
     engine = create_engine(DATABASE_URL, client_encoding="utf8")
